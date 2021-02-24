b8b4a200902a528faa0f642fd5af9df0041a44e8
Cyanide3D <gamer1997235@yandex.ru>
2021-02-24 04:31:28 +0300

    init

---
+++ Assets/UserButtonScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserButtonScript : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.blue;
+    }
+}
